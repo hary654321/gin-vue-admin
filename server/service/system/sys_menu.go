0cdb244a03a6e39b283f27832e95f8999a9ebe23
wangrui <hary654321@126.com>
2023-05-31 09:16:40 +0800

    AuthorityId

--- server/service/system/sys_menu.go
+++ server/service/system/sys_menu.go
@@ -7,6 +7,7 @@ import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
@@ -226,12 +227,14 @@ func (menuService *MenuService) GetMenuAuthority(info *request.GetAuthorityId) (
 //	Author [SliverHorn](https://github.com/SliverHorn)
 func (menuService *MenuService) UserAuthorityDefaultRouter(user *system.SysUser) {
 	var menuIds []string
+	global.GVA_LOG.Info("AuthorityId", zap.Any("user.AuthorityId", user.AuthorityId))
 	err := global.GVA_DB.Model(&system.SysAuthorityMenu{}).Where("sys_authority_authority_id = ?", user.AuthorityId).Pluck("sys_base_menu_id", &menuIds).Error
 	if err != nil {
 		return
 	}
 	var am system.SysBaseMenu
 	err = global.GVA_DB.First(&am, "name = ? and id in (?)", user.Authority.DefaultRouter, menuIds).Error
+	// err = global.GVA_DB.First(&am, "name = ? ", user.Authority.DefaultRouter).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		user.Authority.DefaultRouter = "404"
 	}
